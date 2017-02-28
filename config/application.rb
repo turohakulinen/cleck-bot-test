2bbeec740fbc3cb017947851a6cd5b4c71844b5d
Turo Hakulinen <turo.hakulinen@gmail.com>
2017-02-28 11:38:11 +0000

    fix

--- config/application.rb
+++ config/application.rb
@@ -21,9 +21,7 @@ module CleckBotTest
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.paths.add File.join("app", "bot"), glob: File.join("**","*.rb")
+    config.autoload_paths += Dir[Rails.root.join("app", "bot", "*")]
   end
 end
-
-# Auto-load /bot and its subdirectories
-config.paths.add File.join("app", "bot"), glob: File.join("**","*.rb")
-config.autoload_paths += Dir[Rails.root.join("app", "bot", "*")]
